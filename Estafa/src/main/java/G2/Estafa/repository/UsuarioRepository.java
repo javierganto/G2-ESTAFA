6263ea2ae82546d1c376f12656439f05bb557a86
Pablo <Pablo0gf7@uma.es>
2020-05-28 10:23:13 +0200

    Create UsuarioRepository.java

---
+++ Estafa/src/main/java/G2/Estafa/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package G2.Estafa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import G2.Estafa.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String>{
+
+}
