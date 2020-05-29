a3a83359017a407590888c22ee49e0c9c0372adb
javierganto <javierganto@uma.es>
2020-05-29 22:09:16 +0200

    Update ModeradorService.java

--- Estafa/src/main/java/G2/Estafa/service/ModeradorService.java
+++ Estafa/src/main/java/G2/Estafa/service/ModeradorService.java
@@ -1,5 +1,42 @@
 package G2.Estafa.service;
 
+
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import G2.Estafa.model.Moderador;
+import G2.Estafa.repository.ModeradorRepository;
+
 public class ModeradorService {
+	@Autowired
+	ModeradorRepository moderadorRepository;
+	
+	public List<Moderador> getAll(){
+		return moderadorRepository.findAll();
+	}
+	
+	public void save(Moderador m) {
+		moderadorRepository.saveAndFlush(m);
+	}
+
+	public void delete(String nick) {
+		moderadorRepository.deleteById(nick);
+	}
+
+	public Moderador getById(String nick) {
+		return moderadorRepository.getOne(nick);
+	}
+	
+	
+	public Moderador comprobar(String nick, String pas) {
+        Moderador moderador = moderadorRepository.getOne(nick);
+        if (moderador.getPassword().equals(pas))
+            return moderador;
+        return null;
+   }
 
 }
