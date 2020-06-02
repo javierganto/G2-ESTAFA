19f01656c2a8e1a64fe21e9c41f829969fa54797
Ricardolobo2000 <ricardolobo2000@uma.es>
2020-06-02 11:12:25 +0200

    Update ModeradorController.java

--- Estafa/src/main/java/G2/Estafa/controller/ModeradorController.java
+++ Estafa/src/main/java/G2/Estafa/controller/ModeradorController.java
@@ -14,105 +14,109 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import G2.Estafa.model.Moderador;
 import G2.Estafa.model.Usuario;
 import G2.Estafa.service.ModeradorService;
+import G2.Estafa.service.UsuarioService;
 
+@Controller
 public class ModeradorController {
+		
+	@Autowired
+	ModeradorService moderadorservice;
+	@Autowired
+	UsuarioService usuarioservice;
 	
+	List<Usuario> user;
+	
+	@RequestMapping("/moderadores")
+	public String listadomoderadores(Model model) {
+		List<Usuario> moderadores = new ArrayList<>();
+		Moderador p = new Moderador();
+		p.setNombre("Ricardo");
+		p.setApellidos("Lopez Bordoli");
+		p.setNick("awakate");
+		p.setEmail("ricardolobo2000@hotmail.com");
+		p.setPassword("awakate");
+		p.setValoracion(10);
+		moderadores.add(p);
+		
+		p = new Moderador();
+		p.setNombre("Javier");
+		p.setApellidos("Garcia Antolin");
+		p.setNick("javierganto");
+		p.setEmail("javierganto@gmail.com");
+		p.setPassword("javierganto");
+		p.setValoracion(10);
+		moderadores.add(p);
+		
+		p = new Moderador();
+		p.setNombre("David");
+		p.setApellidos("Lopez Bordoli");
+		p.setNick("awakate");
+		p.setEmail("ricardolobo2000@hotmail.com");
+		p.setPassword("awakate");
+		p.setValoracion(10);
+		moderadores.add(p);
+		
+		p = new Moderador();
+		p.setNombre("Ricardo");
+		p.setApellidos("Lopez Bordoli");
+		p.setNick("awakate");
+		p.setEmail("ricardolobo2000@hotmail.com");
+		p.setPassword("awakate");
+		p.setValoracion(10);
+		moderadores.add(p);
+		
+		p = new Moderador();
+		p.setNombre("Ricardo");
+		p.setApellidos("Lopez Bordoli");
+		p.setNick("awakate");
+		p.setEmail("ricardolobo2000@hotmail.com");
+		p.setPassword("awakate");
+		p.setValoracion(10);
+		moderadores.add(p);
+		
+		model.addAttribute("ListaModeradores", moderadores);
+		
+		return "moderadores/index";
+	}
+	@RequestMapping("/moderadores/login")
+    public String loginmoderadores(String nick, String pas) {
+        moderadorservice.comprobar(nick,pas);
+        return "moderadores/login";
+    }
+	/*
+    @PostMapping("/usuarios/save")
+    public String perfilusuario(Usuario u) {
+        return "redirect:/view";
 
-	@Controller
-	public class UsuarioController {
-		
-		@Autowired
-		ModeradorService moderadorservice;
-		List<Usuario> user;
-		
-		@RequestMapping("/moderadores")
-		public String listadomoderadores(Model model) {
-			List<Usuario> moderadores = new ArrayList<>();
-			Moderador p = new Moderador();
-			p.setNombre("Ricardo");
-			p.setApellidos("Lopez Bordoli");
-			p.setNick("awakate");
-			p.setEmail("ricardolobo2000@hotmail.com");
-			p.setPassword("awakate");
-			p.setValoracion(10);
-			moderadores.add(p);
-			
-			p = new Moderador();
-			p.setNombre("Javier");
-			p.setApellidos("Garcia Antolin");
-			p.setNick("javierganto");
-			p.setEmail("javierganto@gmail.com");
-			p.setPassword("javierganto");
-			p.setValoracion(10);
-			moderadores.add(p);
-			
-			p = new Moderador();
-			p.setNombre("David");
-			p.setApellidos("Lopez Bordoli");
-			p.setNick("awakate");
-			p.setEmail("ricardolobo2000@hotmail.com");
-			p.setPassword("awakate");
-			p.setValoracion(10);
-			moderadores.add(p);
-			
-			p = new Moderador();
-			p.setNombre("Ricardo");
-			p.setApellidos("Lopez Bordoli");
-			p.setNick("awakate");
-			p.setEmail("ricardolobo2000@hotmail.com");
-			p.setPassword("awakate");
-			p.setValoracion(10);
-			moderadores.add(p);
-			
-			p = new Moderador();
-			p.setNombre("Ricardo");
-			p.setApellidos("Lopez Bordoli");
-			p.setNick("awakate");
-			p.setEmail("ricardolobo2000@hotmail.com");
-			p.setPassword("awakate");
-			p.setValoracion(10);
-			moderadores.add(p);
-			
-			model.addAttribute("ListaModeradores", moderadores);
-			
-			return "moderadores/index";
-		}
-		@RequestMapping("/moderadores/login")
-	    public String loginmoderadores(String nick, String pas) {
-	        moderadorservice.comprobar(nick,pas);
-	        return "moderadores/login";
-	    }
-		/*
-	    @PostMapping("/usuarios/save")
-	    public String perfilusuario(Usuario u) {
-	        return "redirect:/view";
-
-	    }*/
-		
-		
-		
-		
-		@RequestMapping("/moderadores/edit/{nick}")
-		public String editmoderadores(@PathVariable("nick") String nick, Model model) {
-			model.addAttribute("moderadores", moderadorservice.getById(nick));
-			return "moderadores/add";
-		}
-		
-		@RequestMapping("/moderadores/view/{nick}")
-		public String viewmoderadores(@PathVariable("nick") String nick, Model model) {
-			model.addAttribute("moderadores", moderadorservice.getById(nick));
-			return "moderadores/view";
-		}
-		@RequestMapping("/moderadores/delete/{nick}")
-		public String deletemoderadores(@PathVariable("nick") String nick) {
-			moderadorservice.delete(nick);
-			return "redirect:/moderadores";
-		}
-		@RequestMapping("/moderadores/delete/usuarios")
-		public String banearUsuario(Usuario u) {
-			moderadorservice.delete(u.getNick());
-			return "redirect:/moderadores";
-		}
+    }*/
+	
+	
+	@RequestMapping("/moderadores/banear/{nick}")
+	public String banearusuario(@PathVariable("nick") String nick) {
+		usuarioservice.delete(nick);
+		return "moderadores";
+	}
+	
+	@RequestMapping("/moderadores/edit/{nick}")
+	public String editmoderadores(@PathVariable("nick") String nick, Model model) {
+		model.addAttribute("moderadores", moderadorservice.getById(nick));
+		return "moderadores/add";
+	}
+	
+	@RequestMapping("/moderadores/view/{nick}")
+	public String viewmoderadores(@PathVariable("nick") String nick, Model model) {
+		model.addAttribute("moderadores", moderadorservice.getById(nick));
+		return "moderadores/view";
+	}
+	@RequestMapping("/moderadores/delete/{nick}")
+	public String deletemoderadores(@PathVariable("nick") String nick) {
+		moderadorservice.delete(nick);
+		return "redirect:/moderadores";
+	}
+	@RequestMapping("/moderadores/delete/usuarios")
+	public String banearUsuario(Usuario u) {
+		moderadorservice.delete(u.getNick());
+		return "redirect:/moderadores";
 	}
 
 
