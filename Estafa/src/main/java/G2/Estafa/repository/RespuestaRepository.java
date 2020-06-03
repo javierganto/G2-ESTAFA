3e24e51c80f0ff082801ec7fc5e076443b630024
javierganto <javierganto@uma.es>
2020-06-03 21:19:22 +0200

    Delete RespuestaRepository.java

--- Estafa/src/main/java/G2/Estafa/repository/RespuestaRepository.java
+++
@@ -1,9 +0,0 @@
-package G2.Estafa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import G2.Estafa.model.Respuesta;
-
-public interface RespuestaRepository extends JpaRepository<Respuesta, String>{
-
-}
