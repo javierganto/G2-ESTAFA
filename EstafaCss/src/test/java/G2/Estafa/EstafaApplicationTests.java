5684ca2293bdeedfa163bc3469e378d849dc6aca
javierganto <javierganto@uma.es>
2020-06-03 22:18:52 +0200

    Create EstafaApplicationTests.java

---
+++ EstafaCss/src/test/java/G2/Estafa/EstafaApplicationTests.java
@@ -0,0 +1,13 @@
+package G2.Estafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
