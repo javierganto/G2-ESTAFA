592e259a1a6fdc7e98283d27bc4c62022702edab
javierganto <javierganto@uma.es>
2020-06-03 22:18:00 +0200

    Create ModeradorRepository.java

---
+++ EstafaCss/src/main/java/G2/Estafa/repository/ModeradorRepository.java
@@ -0,0 +1,10 @@
+package G2.Estafa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import G2.Estafa.model.Moderador;
+
+
+public interface ModeradorRepository extends JpaRepository<Moderador, String>{
+
+}
