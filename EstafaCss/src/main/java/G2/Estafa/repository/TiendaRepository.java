bc3cc1143d7d47ced0a90af672bea85c67970e62
javierganto <javierganto@uma.es>
2020-06-03 22:18:03 +0200

    Create TiendaRepository.java

---
+++ EstafaCss/src/main/java/G2/Estafa/repository/TiendaRepository.java
@@ -0,0 +1,10 @@
+package G2.Estafa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import G2.Estafa.model.Tienda;
+
+
+public interface TiendaRepository extends JpaRepository<Tienda, String> {
+
+}
