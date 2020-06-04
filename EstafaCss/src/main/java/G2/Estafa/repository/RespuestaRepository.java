42c30ebb865ed87566595e410ea4a5946ab75313
javierganto <javierganto@uma.es>
2020-06-04 11:44:00 +0200

    Create RespuestaRepository.java

---
+++ EstafaCss/src/main/java/G2/Estafa/repository/RespuestaRepository.java
@@ -0,0 +1,9 @@
+package G2.Estafa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import G2.Estafa.model.Respuesta;
+
+public interface RespuestaRepository extends JpaRepository<Respuesta, String>{
+
+}
