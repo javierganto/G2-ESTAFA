f346f2b0b1014df2de5574013c690a8736d09496
Ricardolobo2000 <ricardolobo2000@uma.es>
2020-06-06 17:04:39 +0200

    Update TiendaService.java

--- EstafaCss/src/main/java/G2/Estafa/service/TiendaService.java
+++ EstafaCss/src/main/java/G2/Estafa/service/TiendaService.java
@@ -3,12 +3,13 @@ package G2.Estafa.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import G2.Estafa.model.Tienda;
 //import G2.Estafa.model.Usuario;
 import G2.Estafa.repository.TiendaRepository;
 //import G2.Estafa.repository.UsuarioRepository;
-
+@Service
 public class TiendaService {
 	@Autowired
 	TiendaRepository tiendaRepository;
@@ -21,12 +22,12 @@ public class TiendaService {
 		tiendaRepository.saveAndFlush(u);
 	}
 
-	public void delete(String nick) {
-		tiendaRepository.deleteById(nick);
+	public void delete(Integer id) {
+		tiendaRepository.deleteById(id);
 	}
 
-	public Tienda getById(String nick) {
-		return tiendaRepository.getOne(nick);
+	public Tienda getById(Integer id) {
+		return tiendaRepository.getOne(id);
 	}
 	
 	
