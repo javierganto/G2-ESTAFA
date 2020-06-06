13f59282dc3be1a442d87533dda58ade9694a4ce
Pablo <Pablo0gf7@uma.es>
2020-06-06 13:27:22 +0200

    Update RespuestaController.java

--- EstafaCss/src/main/java/G2/Estafa/controller/RespuestaController.java
+++ EstafaCss/src/main/java/G2/Estafa/controller/RespuestaController.java
@@ -3,6 +3,7 @@ package G2.Estafa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import G2.Estafa.model.Respuesta;
 import G2.Estafa.service.RespuestaService;
-
+@Controller
 public class RespuestaController {
 	@Autowired
 	RespuestaService respuestaService;
