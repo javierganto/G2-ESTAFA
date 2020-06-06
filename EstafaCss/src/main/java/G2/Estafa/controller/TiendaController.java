5f7237774e370df74dffc7f8b0bc7e3b61d8e436
Ricardolobo2000 <ricardolobo2000@uma.es>
2020-06-06 17:04:03 +0200

    Update TiendaController.java

--- EstafaCss/src/main/java/G2/Estafa/controller/TiendaController.java
+++ EstafaCss/src/main/java/G2/Estafa/controller/TiendaController.java
@@ -3,56 +3,62 @@ package G2.Estafa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import G2.Estafa.model.Mensaje;
 import G2.Estafa.model.Tienda;
 import G2.Estafa.service.TiendaService;
 //import G2.Estafa.service.UsuarioService;
-
+@Controller
 public class TiendaController {
 	@Autowired
 	TiendaService tiendaservice;
 	
-	@RequestMapping("/tiendas")
+	@RequestMapping("/tienda")
 	public String listadotiendas(Model model) {
 		List<Tienda> tiendas = tiendaservice.getAll();
 		
 		model.addAttribute("ListaTiendas", tiendas);
 		
-		return "tiendas/index";
+		return "tienda/index";
 	}
-	@RequestMapping("/tiendas/add")
+	@RequestMapping("/tienda/add")
 	public String addTienda(Model model) {
 		model.addAttribute("tienda", new Tienda());
-		return "tiendas/add";
+		return "tienda/add";
 	}
 	
 	
 	
 	
-	@PostMapping("/tiendas/save")
+	@PostMapping("/tienda/save")
 	public String saveTienda(Tienda t) {
 		tiendaservice.save(t);
-		return "redirect:/tiendas";
+		return "redirect:/tienda";
 	}
-	@RequestMapping("/tiendas/edit/{nombre}")
-	public String editTienda(@PathVariable("nombre") String nombre, Model model) {
-		model.addAttribute("Tienda", tiendaservice.getById(nombre));
-		return "tiendas/add";
+	@RequestMapping("/tienda/edit/{id}")
+	public String editTienda(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("Tienda", tiendaservice.getById(id));
+		return "tienda/add";
 	}
 	
-	@RequestMapping("/tiendas/view/{nick}")
-	public String viewtiendas(@PathVariable("nick") String nick, Model model) {
-		model.addAttribute("tienda", tiendaservice.getById(nick));
-		return "tiendas/view";
-	}
-	@RequestMapping("/tiendas/delete/{nombre}")
-	public String deleteTiendas(@PathVariable("nombre") String nombre) {
-		tiendaservice.delete(nombre);
-		return "redirect:/tiendas";
+	@RequestMapping("/tienda/view/{id}")
+    public String viewtiendas(@PathVariable("id") Integer id, Model model) {
+        Tienda tienda = tiendaservice.getById(id);
+        model.addAttribute("tienda", tienda);
+        Mensaje mensaje = new Mensaje();
+        mensaje.setTienda(tienda);
+        model.addAttribute("mensaje", mensaje );
+        return "tienda/view";
+    }
+	@RequestMapping("/tienda/delete/{id}")
+	public String deleteTiendas(@PathVariable("id") Integer id) {
+		tiendaservice.delete(id);
+		return "redirect:/tienda";
 	}
 
 }
