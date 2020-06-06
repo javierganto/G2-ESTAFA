ac0e0d456b9ef5d9d2ca9daf5da351d5b9437fd0
Pablo <Pablo0gf7@uma.es>
2020-06-06 13:27:25 +0200

    Update MensajeService.java

--- EstafaCss/src/main/java/G2/Estafa/service/MensajeService.java
+++ EstafaCss/src/main/java/G2/Estafa/service/MensajeService.java
@@ -3,12 +3,13 @@ package G2.Estafa.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import G2.Estafa.model.Mensaje;
 
 import G2.Estafa.repository.MensajeRepository;
 
-
+@Service
 public class MensajeService {
 	@Autowired
 	MensajeRepository mensajerepository;
