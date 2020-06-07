cc6e1617de392b226bce70f1cf300ee6ef028561
javierganto <javierganto@uma.es>
2020-06-07 13:11:18 +0200

    Create ModeradorTest.java

---
+++ EstafaCss/src/test/java/G2/Estafa/model/ModeradorTest.java
@@ -0,0 +1,84 @@
+package G2.Estafa.model;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ModeradorTest {
+	private Usuario usuario;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	
+
+	@Test
+	void testGetNombre() {
+		assertTrue("El nombre no puede ser nulo", (usuario.getNombre()!=null));
+	}
+
+
+	@Test
+	void testGetNacimiento() {
+		assertTrue("Fecha incorrecta", (usuario.getNacimiento()!=null));
+	}
+
+	
+
+	@Test
+	void testGetApellidos() {
+		assertTrue("El apellido no puede ser nulo", (usuario.getApellidos()!=null));
+	}
+
+	
+
+	@Test
+	void testGetNick() {
+		assertTrue("El nick no puede ser nulo", (usuario.getNick()!=null));
+	}
+
+	
+
+	@Test
+	void testGetEmail() {
+		assertTrue("Email no valido", (usuario.getEmail()!=null));
+	}
+
+	
+
+	@Test
+	void testGetPassword() {
+		assertTrue("Contraseña no valida", (usuario.getPassword()!=null));
+	}
+
+	
+
+	
+
+	@Test
+	void testGetValoracion() {
+		assertTrue("La valoracion no puede ser nula", (usuario.getValoracion()>=0||usuario.getValoracion()<=10));
+	}
+
+	
+
+	
+}
