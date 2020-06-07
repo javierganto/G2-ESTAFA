1ec4bb01fa5eb8f616228e8a0c0ee0649096502d
Ricardolobo2000 <ricardolobo2000@uma.es>
2020-06-07 14:30:56 +0200

    Update MensajeController.java

--- EstafaCss/src/main/java/G2/Estafa/controller/MensajeController.java
+++ EstafaCss/src/main/java/G2/Estafa/controller/MensajeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import G2.Estafa.model.Mensaje;
+import G2.Estafa.model.Tienda;
 import G2.Estafa.model.Usuario;
 import G2.Estafa.service.MensajeService;
 import G2.Estafa.service.TiendaService;
@@ -54,11 +55,33 @@ public class MensajeController {
 		model.addAttribute("listaTiendas", tiendaservice.getAll());
 		return "mensajes/add";
 	}
+	@RequestMapping("/mensajes/view/{id}")
+    public String viewmensajes(@PathVariable("id") Integer id, Model model) {
+        Mensaje mensaje = mensajeservice.getById(id);
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("listatiendas", tiendaservice.getAll() );
+		model.addAttribute("listausuarios", usuarioservice.getAll());
+        return "mensajes/view";
+    }
 	
 	@RequestMapping("/mensajes/delete/{id}")
 	public String deletemensajes(@PathVariable("id") Integer id) {
 		mensajeservice.delete(id);
 		return "redirect:/mensajes";
 	}
+	
+	@RequestMapping("/mensajes/neg/{id}")
+    public String negativa(@PathVariable("id") Integer id) {
+        mensajeservice.neg(id);
+
+        return "redirect:/mensajes/view/{id}";
+    }
+	
+	@RequestMapping("/mensajes/pos/{id}")
+    public String positiva(@PathVariable("id") Integer id) {
+        mensajeservice.pos(id);
+
+        return "redirect:/mensajes/view/{id}";
+    }
 	
 }
