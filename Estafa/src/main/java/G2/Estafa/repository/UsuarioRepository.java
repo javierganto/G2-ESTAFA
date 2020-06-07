65374803f805a08ab425d622c7b5d6b84a646ea6
Ricardolobo2000 <ricardolobo2000@uma.es>
2020-06-07 12:16:49 +0200

    Delete UsuarioRepository.java

--- Estafa/src/main/java/G2/Estafa/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package G2.Estafa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import G2.Estafa.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, String>{
-
-}
