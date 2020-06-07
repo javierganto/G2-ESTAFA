f6bd3847d37485d689fd6a897d4c8d4cd7e0643c
Ricardolobo2000 <ricardolobo2000@uma.es>
2020-06-07 14:31:05 +0200

    Update MensajeService.java

--- EstafaCss/src/main/java/G2/Estafa/service/MensajeService.java
+++ EstafaCss/src/main/java/G2/Estafa/service/MensajeService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import G2.Estafa.model.Mensaje;
-
+import G2.Estafa.model.Tienda;
 import G2.Estafa.repository.MensajeRepository;
 
 @Service
@@ -27,6 +27,29 @@ public class MensajeService {
 	}
 	public Mensaje getById(Integer id) {
 		return mensajerepository.getOne(id);
+	}
+	
+	public void neg(Integer id) {
+		 try {
+		        Mensaje mensaje = mensajerepository.getOne(id);
+		        mensaje.neg();
+		        mensajerepository.saveAndFlush(mensaje);
+		        }
+		        catch(Exception e){
+
+		        }
+		
+	}
+	public void pos(Integer id) {
+		 try {
+		        Mensaje mensaje = mensajerepository.getOne(id);
+		        mensaje.pos();
+		        mensajerepository.saveAndFlush(mensaje);
+		        }
+		        catch(Exception e){
+
+		        }
+		
 	}
 
 
