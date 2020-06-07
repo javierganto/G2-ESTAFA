7d71ee38d42af33628f51be903a8a03f737d7404
javierganto <javierganto@uma.es>
2020-06-07 13:19:28 +0200

    Create RespuestaTest.java

---
+++ EstafaCss/src/test/java/G2/Estafa/model/RespuestaTest.java
@@ -0,0 +1,69 @@
+package G2.Estafa.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RespuestaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetContenido() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetContenido() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAutor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsValidez() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetValidez() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetValoracion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
